06d4fa60e136ad0b872740e879fa40792e322048
Jacob Goodall <jraygoodall@gmail.com>
2024-06-26 12:53:42 -0700

    changed to name

--- app/index.tsx
+++ app/index.tsx
@@ -11,6 +11,7 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>Hello World!!</Text>
+      <Text>Hello Scout!!</Text>
     </View>
   );
 }
